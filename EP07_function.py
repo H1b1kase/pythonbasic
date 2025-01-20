86a61a644c8be39b5312acb5e966a3e925b80b16
H1b1kase <wanicam0105@naver,cin>
2025-01-20 11:45:49 +0900

    EP06 comitt

---
+++ EP07_function.py
@@ -0,0 +1,41 @@
+
+# 함수란??
+# 특정 작업을 수행하는 코드의 묶음
+# 모듈화를 통해 재사용성을 높이는 코드
+
+#def greeting(name):
+    #print("안녕하세요 " + name + " 님!")
+
+#greeting("영수")
+#greeting("영철")
+#greeting("광수")
+#greeting("영희")
+
+
+#def add_numbers(num1, num2):
+#    sum = num1 + num2
+#    return sum
+#
+#result = add_numbers(3, 5)
+#print(result)
+
+num1 = 10
+num2 = 20
+num3 = 5
+num4 = 3
+num5 = 15
+num6 =3
+
+add_numbers(num1, num2)
+add_numbers(num1, num3)
+multiply_numbers(num3, num4)
+multiply numbers(num3, num6)
+add_numbers(num4, num5)
+divide_numbers(num5, num6)
+
+
+
+
+
+
+
